d82a01d2174351c3d997f75b2746d153e7ee9221
pandey-hariom-dev <pandey.hariom.dev@gmail.com>
2026-05-31 12:46:18 +0530

    Structured Code Formatting

--- iss_tracker.py
+++ iss_tracker.py
@@ -2,8 +2,8 @@ import requests
 import folium
 import webbrowser
 import time
+
 def position():
-    # Get ISS position
     response = requests.get("http://api.open-notify.org/iss-now.json")
     data = response.json()
     latitude = float(data['iss_position']['latitude'])
@@ -12,21 +12,17 @@ def position():
     print(location)
     print(f"🛸 ISS is at: {latitude}, {longitude}")
     return latitude, longitude
+
 position()
 webbrowser.open("iss_map.html")
+
 while True:
     latitude, longitude = position() 
-    # Create map
     map = folium.Map(location=[latitude, longitude], zoom_start=3)
 
-    # Add ISS marker
-    folium.Marker(
-        location=[latitude, longitude],
-        popup="🛸 ISS is here!",
-        tooltip="International Space Station"
-    ).add_to(map)
 
-    # Save and open in browser
+    folium.Marker(location=[latitude, longitude],popup="🛸 ISS is here!",tooltip="International Space Station").add_to(map)
+
     map.save("iss_map.html")
 
     with open("iss_map.html", "r") as f:
